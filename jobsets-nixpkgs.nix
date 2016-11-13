67ef91644ea09a53c59f998dda6607460dae3432
Pierre Radermecker <pradermecker@cirb.brussels>
2016-11-13 17:29:45 +0100

    testing

--- jobsets-nixpkgs.nix
+++ jobsets-nixpkgs.nix
@@ -11,11 +11,6 @@ with (import <nixpkgs/pkgs/top-level/release-lib.nix> { inherit supportedSystems
   hello_world_2 = pkgs.lib.hydraJob pkgs_x86_64_linux.hello;
 
   # Generate one attribute per supported platform.
-  hello_world_3 = pkgs.lib.genAttrs supportedSystems (system: (pkgsFor system).hello);
-
-} // mapTestOn {
-
-  # Fancy shortcut to generate one attribute per supported platform.
-  hello = supportedSystems;
+  # hello_world_3 = pkgs.lib.genAttrs supportedSystems (system: (pkgsFor system).hello);
 
 }
